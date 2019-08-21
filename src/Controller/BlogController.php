91a4cdbe5e226a43b7114462fce65c31cebc77ed
Jerome colin <jerome.colin64100@gmail.com>
2019-08-21 16:14:29 +0200

    slug

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,24 @@
+<?php
+// src/Controller/BlogController.php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+class BlogController extends AbstractController
+{
+   /**
+    * @Route("/blog/list/{page}", name="blog_list")
+    */
+   public function list($page)
+   {
+       return $this->render('/list.html.twig', ['page' => $page]);
+   }
+   /**
+    * @Route("/blog/show/{slug}", requirements={"slug"="[a-z0-9\-]+"} , defaults={"slug"="article-sans-titre"}, name="blog_slug")
+    *
+    */
+   public function show($slug)
+   {
+       $slug = ucwords(str_replace('-', ' ', $slug) );
+       return $this->render('/show.html.twig' ,  ['slug' => $slug]);
+   }
+}
